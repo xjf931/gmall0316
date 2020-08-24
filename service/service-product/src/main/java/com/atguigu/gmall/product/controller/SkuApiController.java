6749962081ed62790b56210e8192e4625d9c949b
xjf <634217531@qq.com>
2020-08-24 18:59:45 +0800

    8/24 commit

---
+++ service/service-product/src/main/java/com/atguigu/gmall/product/controller/SkuApiController.java
@@ -0,0 +1,42 @@
+package com.atguigu.gmall.product.controller;
+
+import com.atguigu.gmall.common.result.Result;
+import com.atguigu.gmall.model.product.SkuInfo;
+import com.atguigu.gmall.model.product.SpuInfo;
+import com.atguigu.gmall.product.service.SkuInfoService;
+import com.baomidou.mybatisplus.core.metadata.IPage;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("admin/product")
+@CrossOrigin
+public class SkuApiController {
+
+	@Autowired
+	SkuInfoService skuInfoService;
+
+	@RequestMapping("cancelSale/{skuId}")
+	public Result cancelSale(@PathVariable String skuId){
+		skuInfoService.cancelSale(skuId);
+		return Result.ok();
+	}
+
+
+	@RequestMapping("onSale/{skuId}")
+	public Result onSale(@PathVariable String skuId){
+		skuInfoService.onSale(skuId);
+		return Result.ok();
+	}
+
+
+	@RequestMapping("list/{page}/{limit}")
+	public Result list(@PathVariable("page") Long page, @PathVariable("limit") Long limit){
+		Page<SkuInfo> pageParam = new Page<>(page, limit);
+		IPage<SkuInfo> skuInfoIPage = skuInfoService.list(pageParam);
+		return Result.ok(skuInfoIPage);
+	}
+
+
+}
